a30a786f29c051e74be988e74013a360b3c935dc
SHD yyds <wuyoualan@126.com>
2021-08-16 17:50:00 +0800

    Create 找质数.py

---
+++ 找质数.py
@@ -0,0 +1,9 @@
+for i in range(2,10000):
+     for j in range(3,10):
+          while i % j != 0:
+               j = j + 1
+               if j > int(j**0.5):
+                    print(i)
+               
+               
+    
